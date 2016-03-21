c6f765a8d6ebd46454d72ab668211e361dbf0c1e
Jaclyn Adams <jaadams05@gmail.com>
2016-03-20 18:13:27 -0700

    finalize script to print out names and roles from about us

--- about-us-parser/index.js
+++ about-us-parser/index.js
@@ -1,9 +1,5 @@
 var request = require("request");
 var cheerio = require("cheerio");
-var express = require("express");
-var fs = require("fs");
-
-var scraper = express();
 
 var url = "https://prism.com/about/";
 
@@ -12,26 +8,17 @@ request(url, function(err, response, html){
 	if(!err){
 		var $ = cheerio.load(html);
 		
-		var people = $(".team-grid .offset1b li");
-		console.log("There are " + people.length + " people on the Prsim team. They are:");
+		var people = $(".team-grid .offset1b li span");
+		console.log("There are " + people.length + " people on the Prsim team. They are:\n");
 
-		// people.each(function(i, person){
-		// 	console.log(person.children);
-		// });
-		// var vars; 
-		// var json = {};
-		// people("p").map(function(i, person){
-		// 	var 
-		// });
+		people.each(function(i, person){
+			var name = $(this).children('p').first();
+			var role = name.next();
+			console.log(name.text() + ", " + role.text());
+		});
+
 	}
-})
-
-
-//scraper.listen("8000");
-
-//exports = module.exports = scraper; 
-
-
-//class team-grid, div 
-//class offset1b
-//each li, first <p> = name, second <p> = role
+	else{
+		console.log("Encountered error: " + err);
+	}
+});
